dc10edc84b33d0f05abd71ed0b64892806a701a3
melon-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2022-03-31 03:54:04 +0900

    Make profile table responsive

--- src/components/profile-information.tsx
+++ src/components/profile-information.tsx
@@ -1,4 +1,5 @@
 import { Table, Tbody, Tr, Td, Heading, Center } from '@chakra-ui/react'
+import { Link } from '@chakra-ui/react'
 
 export default function ProfileInformation() {
   return (
@@ -8,32 +9,48 @@ export default function ProfileInformation() {
           Houshou Marine
         </Heading>
       </Center>
-      <Table variant="simple" colorScheme="facebook" size="sm" marginBottom={3} marginX={2}>
-        <Tbody>
-          <Tr>
-            <Td>Birthday</Td>
-            <Td>July 30th</Td>
-          </Tr>
-          <Tr>
-            <Td>First Stream</Td>
-            <Td>August 11th, 2019</Td>
-          </Tr>
-          <Tr>
-            <Td>Height</Td>
-            <Td>150cm</Td>
-          </Tr>
-          <Tr>
-            <Td>Hashtags</Td>
-            <Td>
-              Stream: #マリン航海記 <br /> Fan Art: #マリンのお宝
-            </Td>
-          </Tr>
-          <Tr>
-            <Td>Blood Type</Td>
-            <Td>O</Td>
-          </Tr>
-        </Tbody>
-      </Table>
+      <Center>
+        <Table
+          variant="simple"
+          colorScheme="facebook"
+          size="sm"
+          marginBottom={3}
+          marginX={2}
+          width={['100%', '60%']}
+        >
+          <Tbody>
+            <Tr>
+              <Td>Birthday</Td>
+              <Td>July 30th</Td>
+            </Tr>
+            <Tr>
+              <Td>First Stream</Td>
+              <Td>August 11th, 2019</Td>
+            </Tr>
+            <Tr>
+              <Td>Height</Td>
+              <Td>150cm</Td>
+            </Tr>
+            <Tr>
+              <Td>Hashtags</Td>
+              <Td>
+                Stream:
+                <Link
+                  isExternal={true}
+                  href="https://twitter.com/search?q=%23%E3%83%9E%E3%83%AA%E3%83%B3%E8%88%AA%E6%B5%B7%E8%A8%98&src=typed_query"
+                >
+                  #マリン航海記
+                </Link>
+                <br /> Fan Art: #マリンのお宝
+              </Td>
+            </Tr>
+            <Tr>
+              <Td>Blood Type</Td>
+              <Td>O</Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </Center>
     </div>
   )
 }
